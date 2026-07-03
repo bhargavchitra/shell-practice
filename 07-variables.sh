88592e405fa4ed8228b1e60a34e390f168601da5
bhargavchitra <bhargavchitra05.com>
2026-07-03 19:40:23 +0530

    actions

--- 07-variables.sh
+++ 07-variables.sh
@@ -5,8 +5,8 @@
 echo "All args passed to script: $@"
 echo "Number of variables passed to script: $#"
 echo "Script name: $0 "
-echo "Present which directory you are in: $pwd"
-echo "Who is running this script: $USER"
+echo "Present  directory: $PWD"
+echo "Who is running: $USER"
 echo "Home directory of current user: $Home"
 echo "PID of this script: $$"
 sleep 100 &
