cde1d2e7193e19f7c1f8d7d58afc09a8e98dc1a1
bhargavchitra <bhargavchitra05.com>
2026-07-03 13:51:56 +0530

    actions

---
+++ 03-variables.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PERSON1=Suresh # no space between value and = 
+PERSON2=Ramesh 
+
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo  "$PERSON2:: Hi $PERSON1, I am Fine thanks for asking. How are you doning?"
+echo "$PERSON1:: doing fine. what are you learning $PERSON2?"
+echo "$PERSON2:: I am learning shell. what about you?" 
+
+
+
+
+
+
+
+
+
+
+
+
