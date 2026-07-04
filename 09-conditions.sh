0e5353321f714ce48f2880bcb746a4e4be6f8919
bhargavchitra <bhargavchitra05.com>
2026-07-04 14:41:25 +0530

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -7,5 +7,5 @@ NUMBER=$1
 # -eq -> equal
 # -ne -> not equal 
 if [ $NUMBER -gt 20]; then
-    echo "Given number: $NUMBER is greater than 25"
+    echo "Given number: $NUMBER is greater than 20"
 fi
