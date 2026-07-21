56d442e624063ca1deb1776402ee90a6148aa962
bhargavchitra <bhargavchitra05.com>
2026-07-21 18:18:37 +0530

    action

---
+++ 11-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root user or with sudo"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo "$1  ... FAILURE"
+        exit 1
+  else
+       echo "$2   ... success"
+ fi
+}
+
+dnf install nginx -y 
+VALIDATE $? "Installing Nginx"
+
+
+dnf install mysql -y
+VALIDATE $?  "Installing MySQL"
+
+dnf install nodejs -y 
+VALIDATE $? "Installing Node.js"
+
+
+
