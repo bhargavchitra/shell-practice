1bce2d5e7f9348a779615228dac99358ab3a0ace
bhargavchitra <bhargavchitra05.com>
2026-07-21 17:52:23 +0530

    action

--- 10-installation.sh
+++ 10-installation.sh
@@ -6,9 +6,9 @@ if [ $USERID -ne 0 ]; then
     echo "Please run this script as root user or with sudo"
     exit 1
 fi
-
 echo "Installing Nginx"
 dnf install nginx -y 
+
 
 if [ $? -ne 0 ]; then
     echo "Installing  Nginx  ... FAILURE"
@@ -16,3 +16,23 @@ if [ $? -ne 0 ]; then
 else
     echo "Installing Nginx ... success"
 fi
+
+
+
+dnf install mysql-y
+if [ $? -ne 0 ]; then
+    echo "Installing  MySQL  ... FAILURE"
+    exit 1
+else
+    echo "Installing MySQL ... success"
+fi
+
+
+dnf install nodejs -y 
+if [ $? -ne 0 ]; then
+    echo "Installing  Node.js  ... FAILURE"
+    exit 1
+else
+    echo "Installing Node.js ... success"
+fi
+
