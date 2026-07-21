dfa1e426ab6d90a8e099ec76b24748e0bdff6e42
bhargavchitra <bhargavchitra05.com>
2026-07-21 13:23:05 +0530

    action

--- 06-variables.sh
+++ 06-variables.sh
@@ -10,8 +10,10 @@ echo "Script executed at: $TIMESTAMP"
 sleep 10
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$((END_TIME-$START_TIME))
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 echo "Script executed in: $"
+
+
 
 
 
