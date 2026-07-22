a33e9fa68c4c7c051e2e85c6c7a728c9d9978b16
bhargavchitra <bhargavchitra05.com>
2026-07-22 12:44:03 +0530

    action

---
+++ 12-logs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash 
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run this script as root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+        echo "$1  ... FAILURE"
+        exit 1
+  else
+       echo "$2   ... success"
+ fi
+}
+
+dnf install nginx -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nginx"
+
+
+dnf install mysql -y &>> $LOGS_FILE
+VALIDATE $?  "Installing MySQL"
+
+dnf install nodejs -y &>> $LOGS_FILE
+VALIDATE $? "Installing Nodejs"
+
+
