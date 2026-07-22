b676ad8d4e4f1cba892d7a78d7c2ad8a661d1a9b
bhargavchitra <bhargavchitra05.com>
2026-07-22 17:40:35 +0530

    action

--- 19-trap.sh
+++ 19-trap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-set -e
+set -e #ERR 
+
+
+trap 'echo  "There is an error in $LIENO, Command: $BASH_COMMAND"' ERR
 
 echo "Hello World"
 echo "i am learning shell"
 echoo "printing error here"
 echo "no error "
+
 
 
 
