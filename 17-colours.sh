c3dbcea4b591655f5abc88121ebc5c340f08868b
bhargavchitra <bhargavchitra05.com>
2026-07-22 17:10:38 +0530

    action

--- 17-colours.sh
+++ 17-colours.sh
@@ -36,7 +36,7 @@ do
          dnf install $package -y &>> $LOGS_FILE
          VALIDATE $? "$package installation"
     else
-        echo "$package already installed, $G skipping $N"
+        echo "$package already installed, $Y skipping $N"
     fi
 done
 
