b9d6ae41c678bde4fac0a3ca83774af149b2a100
bhargavchitra <bhargavchitra05.com>
2026-07-22 11:18:28 +0530

    action

--- 10-installation.sh
+++ 10-installation.sh
@@ -1,8 +1,12 @@
 #!/bin/bash 
 
 USERID=$(id -u)
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo "please run this script as root user or with sudo"
 fi
 echo "Installing Nginx"
 dnf install nginx -y 
+
+
+
+
