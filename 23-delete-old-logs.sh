03070e20778a823be9125c51c95da84d7f48894c
bhargavchitra <bhargavchitra05.com>
2026-08-01 22:56:03 +0530

    actions

--- 23-delete-old-logs.sh
+++ 23-delete-old-logs.sh
@@ -10,3 +10,11 @@ fi
 
 FILE_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
 echo "$FILES_TO_DELETE"
+
+while IFS= read -r filepath; do
+ # process each line here 
+   echo "$filepath"
+   #echo "Deleting file: $filepath"
+  # rm -f $filepath
+   #echo "Deleting file: $filepath"
+done <<< $FILES_TO_DELETE
