200b1f43cf6bd81bb7bae18264a650d1b7a3f45f
bhargavchitra <bhargavchitra05.com>
2026-08-02 14:13:33 +0530

    actions

---
+++ 25-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/backup.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script as root user access $N" 
+fi
+
+mkdir -p $LOGS_FOLDER
+
+USAGE(){
+    echo -e "sudo backup <SOURCE_DEL> <DEST_DIR> <DAYS>[default 14 days]"
+    exit 1 
+}
+
+if [ $# -lt 2 ]; then
+   USAGE 
+fi 
+
