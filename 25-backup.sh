e37c85043fd594c038657f683cff817aae3e2795
bhargavchitra <bhargavchitra05.com>
2026-08-02 17:55:28 +0530

    actions

--- 25-backup.sh
+++ 25-backup.sh
@@ -1,80 +1,77 @@
 #!/bin/bash
 
-
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/backup.log"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-B="\e[34m"
-M="\e[35m"
-C="\e[36m"
-W="\e[37m"
 N="\e[0m"
-SOURCE_DEL=$1
-DEST_DIR=$2 
-DAYS=${3:-14} # 14 days is the default value, if the user not supplied 
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14} # 14 days is the default value, if the user not supplied
+
+log(){
+    echo -e "$(date "+%Y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script as root user access $N" 
+    echo -e "$R Please run this script with root user access $N"
+    exit 1
 fi
 
 mkdir -p $LOGS_FOLDER
 
 USAGE(){
-    echo -e "sudo backup <SOURCE_DEL> <DEST_DIR> <DAYS>[default 14 days]"
-    exit 1 
+    log "$R USAGE:: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS>[default 14 days] $N"
+    exit 1
 }
 
-log(){
-    echo -e "$(date "+%y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
-}
 
 if [ $# -lt 2 ]; then
-   USAGE 
-fi 
+    USAGE
+fi
 
-if [ ! -d $SOURCE_DIR ]; then 
-     echo -e "source directory: $SOURCE_DIR does not exist" 
-     exit 1
-fi 
+if [ ! -d "$SOURCE_DIR" ]; then
+    log "$R Source Directory: $SOURCE_DIR does not exist $N"
+    exit 1
+fi
 
-if [ ! -d $DEST_DIR ]; then 
-     echo -e " destination directory:  $DEST_DIR does not exist" 
-     exit 1
-fi 
+if [ ! -d "$DEST_DIR" ]; then
+    log "$R Destination Directory:  $DEST_DIR does not exist $N"
+    exit 1
+fi
 
- # find the files
-FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+### Find the files
+FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +$DAYS)
 
-log "Backup started" 
-log "source Directory: $SOURCE_DIR"
+log "Backup started"
+log "Source Directory: $SOURCE_DIR"
 log "Destination Directory: $DEST_DIR"
-log "Days: $DAYS" 
+log "Days: $DAYS"
 
-if [ -z "{$FILES}" ]; then 
-    log "No files to archieve...$Y skipping $Y" 
-else 
-   # app-logs-$timestamp.zip
-   log "files found to archieve: $FILES"
-   TIMESTAMP=$(date +%F-%H-%M-%S)
-   ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
-   echo "Archieve name: $ZIP_FILE_NAME"
-   tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+if [ -z "${FILES}" ]; then
+    log "No files to archieve ... $Y Skipping $N"
+else
+    # app-logs-$timestamp.zip
+    log "Files found to archieve: $FILES"
+    TIMESTAMP=$(date +%F-%H-%M-%S)
+    ZIP_FILE_NAME="$DEST_DIR/app-logs-$TIMESTAMP.tar.gz"
+    log "Archieve name: $ZIP_FILE_NAME"
+    tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
-   # check archieve is success or not 
-    if [ -f $ZIP_FILE_NAME ]; then 
-       log "Archeival is.. $G success $G"
+    # Check archieve is success or not
+    if [ -f $ZIP_FILE_NAME ]; then
+        log "Archeival is ... $G SUCCESS $N"
 
-       while IFS= read -r filepath; do
-       # process each line here 
-       echo "Deleting file: $filepath"
-       rm -f $filepath
-       echo "Deleting file: $filepath"
-       done <<< $FILES
+        while IFS= read -r filepath; do
+        # Process each line here
+        log "Deleting file: $filepath"
+        rm -f $filepath
+        log "Deleted file: $filepath"
+        done <<< $FILES
     else
-       log "Archeival is ... $R FAILURE $R" 
-       exit 1
-    fi 
-fi 
+        log "Archeival is ... $R FAILURE $N"
+        exit 1
+    fi
+fi
